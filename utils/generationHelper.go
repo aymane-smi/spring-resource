0ea08ed73a1ed6a37211d0c210cfcb98e1aee08b
belassiria aymane <aymanebel2@outlook.fr>
2024-04-30 10:08:14 +0100

    edit GenerateTree

--- utils/generationHelper.go
+++ utils/generationHelper.go
@@ -2,18 +2,27 @@ package utils
 
 import (
 	"os"
-	"strings"
 )
 
 func GenerateTree(path string) bool {
 	//prototype function
 	//works only on array of two ["path", "subfolder1"]
-	slices := strings.Split(path, "/")
-	if err := os.MkdirAll(slices[0], 0755); err != nil {
+	// slices := strings.Split(path, "/")
+	// var treeBuilder string
+	// for i:=0;i<len(slices);i++{
+	// 	treeBuilder += slices[i]
+
+	// }
+	// if err := os.MkdirAll(slices[0], 0755); err != nil {
+	// 	return false
+	// } else if err := os.MkdirAll(slices[0]+"/"+slices[1], 0755); err != nil {
+	// 	return false
+	// }
+	if err := os.MkdirAll(path, 0755); err != nil {
 		return false
-	} else if err := os.MkdirAll(slices[0]+"/"+slices[1], 0755); err != nil {
-		return false
+	} else {
+		os.Chmod(path, 0755|os.ModeSetuid)
+		return true
 	}
-	os.Chmod(slices[0]+"/"+slices[1], 0755|os.ModeSetuid)
-	return true
+
 }
