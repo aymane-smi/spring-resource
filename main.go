b782e9dca5dadb7f45b62c4ddfa3716d98327702
belassiria aymane <aymanebel2@outlook.fr>
2024-04-30 13:25:56 +0100

    adding project helper method(project check/maven info extractor)

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	service "github.com/aymane-smi/spring-resource/services"
-	"github.com/aymane-smi/spring-resource/structs"
+	"fmt"
+
+	"github.com/aymane-smi/spring-resource/utils"
 )
 
 func main() {
-	shared := &structs.Shared{
-		SharedEntity: structs.Entity{
-			Name:     "User",
-			TypeId:   1,
-			RepoType: "JpaRepository",
-		},
-		SharedPom: structs.Pom{
-			GroupId:    "com.example",
-			ArtifactId: "demo",
-		},
-	}
-	service.GenerateRepository(*shared)
-	service.GenerateEntity(*shared)
-	service.GenerateService(*shared)
-	service.GenerateServiceImpl(*shared)
+	// shared := &structs.Shared{
+	// 	SharedEntity: structs.Entity{
+	// 		Name:     "User",
+	// 		TypeId:   1,
+	// 		RepoType: "JpaRepository",
+	// 	},
+	// 	SharedPom: structs.Pom{
+	// 		GroupId:    "com.example",
+	// 		ArtifactId: "demo",
+	// 	},
+	// }
+	// service.GenerateRepository(*shared)
+	// service.GenerateEntity(*shared)
+	// service.GenerateService(*shared)
+	// service.GenerateServiceImpl(*shared)
+	fmt.Println(utils.IsJavaProject("/Users/macbookair/Desktop/YouStream/demo/pom.xml"))
+	//fmt.Println(utils.GenerateProjectInfo("/Users/macbookair/Desktop/YouStream/demo/pom.xml"))
 }
