9345af4c47e60c1f74c73a01f616edcc6fdcb6f3
belassiria aymane <aymanebel2@outlook.fr>
2024-06-15 23:38:06 +0100

    #12 add kotlin support to service

--- services/ServiceService.go
+++ services/ServiceService.go
@@ -10,13 +10,25 @@ import (
 	"github.com/aymane-smi/spring-resource/utils"
 )
 
-func GenerateService(shared structs.Shared, path string) (bool, error) {
-	tmpl, _ := template.ParseFiles("static/service.tmpl")
+func GenerateService(shared structs.Shared, path string, templType int) (bool, error) {
+	var tmpl *template.Template
+	var folderName, extension string
+
+	if templType == 1 {
+		tmpl, _ = template.ParseFiles("static/entity.tmpl")
+		folderName = "java"
+		extension = ".java"
+	} else {
+		tmpl, _ = template.ParseFiles("static/kotlin/entity.tmpl")
+		folderName = "kotlin"
+		extension = ".kt"
+	}
 	pomToPath := strings.ReplaceAll(shared.SharedPom.GroupId, ".", "/") + "/"
-	if !utils.GenerateTree(path + "/src/main/java/" + pomToPath + shared.SharedPom.ArtifactId + "/Services") {
+
+	if !utils.GenerateTree(path + "/src/main/" + folderName + "/" + pomToPath + shared.SharedPom.ArtifactId + "/Services") {
 		return false, errors.New("can't create folder or subfolder")
 	}
-	file, errFile := os.Create(path + "/src/main/java/" + pomToPath + shared.SharedPom.ArtifactId + "/Services/" + shared.SharedEntity.Name + "Service.java")
+	file, errFile := os.Create(path + "/src/main/" + folderName + "/" + pomToPath + shared.SharedPom.ArtifactId + "/Services/" + shared.SharedEntity.Name + "Service" + extension)
 	if errFile != nil {
 		return false, errFile
 	}
@@ -25,14 +37,25 @@ func GenerateService(shared structs.Shared, path string) (bool, error) {
 	return true, nil
 }
 
-func GenerateServiceImpl(shared structs.Shared, path string) (bool, error) {
+func GenerateServiceImpl(shared structs.Shared, path string, templType int) (bool, error) {
+	var tmpl *template.Template
+	var folderName, extension string
+
+	if templType == 1 {
+		tmpl, _ = template.ParseFiles("static/entity.tmpl")
+		folderName = "java"
+		extension = ".java"
+	} else {
+		tmpl, _ = template.ParseFiles("static/kotlin/entity.tmpl")
+		folderName = "kotlin"
+		extension = ".kt"
+	}
 	pomToPath := strings.ReplaceAll(shared.SharedPom.GroupId, ".", "/") + "/"
-	tmpl, _ := template.ParseFiles("static/serviceImpl.tmpl")
-	if !utils.GenerateTree(path + "/src/main/java/" + pomToPath + shared.SharedPom.ArtifactId + "/Services/Impl") {
+	if !utils.GenerateTree(path + "/src/main/" + folderName + "/" + pomToPath + shared.SharedPom.ArtifactId + "/Services/Impl") {
 		println("can't create subfolder in service impl")
 		return false, errors.New("can't create folder or subfolder")
 	}
-	file, errFile := os.Create(path + "/src/main/java/" + pomToPath + shared.SharedPom.ArtifactId + "/Services/Impl/" + shared.SharedEntity.Name + "ServiceImpl.java")
+	file, errFile := os.Create(path + "/src/main/" + folderName + "/" + pomToPath + shared.SharedPom.ArtifactId + "/Services/" + shared.SharedEntity.Name + "ServiceImpl" + extension)
 	if errFile != nil {
 		return false, errFile
 	}
